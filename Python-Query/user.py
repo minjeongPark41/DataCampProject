61166a514df2d0f6a7a97d65f93c504c3fe15b40
minjeongPark41 <pmjabcd@gmail.com>
2022-10-29 11:18:47 +0900

    파이썬으로 DB 연결해서 가지고 오기 - class User 생성

--- Python-Query/user.py
+++ Python-Query/user.py
@@ -1,3 +1,12 @@
 import pymysql
 
 # Module which justifies classes and actions about 'User'
+
+'''
+    user_id : User's MK (Main Key)
+    course_name : 'Data Analysis'
+    name : person name
+    current_rented_books: how many books the person rent
+    rent_free_date : The day person can borrow books 'again' after delay of renting books
+    rentable : status whether person can borrow or not
+'''
